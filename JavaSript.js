1a8350ae9eb12c74c969bb06192504225d0d5626
Gonza <gonzanunez0408@gmail.com>
2024-03-06 16:27:02 -0300

    agregar servicio de listado

--- JavaSript.js
+++ JavaSript.js
@@ -1,3 +1,28 @@
+const fetchAgendaListar = async () => {
+  try {
+    const response = await fetch(
+      "https://bomberosalerta.com.ar/cursoweb/servicioAgendaListar"
+    );
+    const data = await response.json();
+    console.log(data);
+    let body = "";
+    for (let i = 0; i < data.length; i++) {
+      body += `<tr><td><button onclick='setear(this)'>Editar</button></td>
+                  <td>${data[i].CONTACTO} </td>
+                  <td>${data[i].TELEFONO} </td>
+                  <td>${data[i].FECHA_MODIFICACION} </td>
+                  <td><button onclick='borrarFila(this)'>Borrar</button></td>
+                  </tr>
+                  `;
+    }
+    document.getElementById("tbody").innerHTML = body;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchAgendaListar();
+
 function setear() {
   const nombre = document.getElementById("nom").value;
   const numero = document.getElementById("num").value;
