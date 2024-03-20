6206f601a27c33792c1d992d6a3669cc9f966a7e
Gonza <gonzanunez0408@gmail.com>
2024-03-20 17:44:32 -0300

    Trabajo escuela

---
+++ Proyecto-esc/Javascript.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 100) {
+    document.querySelector(".gotop-cont").classList.add("show");
+  } else {
+    document.querySelector(".gotop-cont").classList.remove("show");
+  }
+};
+
+document.querySelector(".gotop-cont").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
