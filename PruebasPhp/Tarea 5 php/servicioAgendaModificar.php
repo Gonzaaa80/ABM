2cb2293eb6a08b72ead0dd8763140a032d23a333
Gonza <gonzanunez0408@gmail.com>
2024-03-21 16:19:40 -0300

    Corregido error de modificar

--- PruebasPhp/Tarea 5 php/servicioAgendaModificar.php
+++ PruebasPhp/Tarea 5 php/servicioAgendaModificar.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['ID_USUARIO']) && !empty($_SESSION['ID_USUARIO'])){
     try {
         $db = new mysqli("127.0.0.1", "u922728743_gonza_usr", "Gonza#123", "u922728743_gonza_db");
     
-        $db->query("UPDATE ABM SET CONTACTO ='$contacto', TELEFONO ='$telefono'  WHERE ID_CONTACTO = '$idContacto' WHERE ID_USUARIOABM = $id " );
+        $db->query("UPDATE ABM SET CONTACTO ='$contacto', TELEFONO ='$telefono'  WHERE ID_CONTACTO = '$idContacto' AND ID_USUARIOABM = $id " );
         echo "Modificado OK";
     } catch (Throwable $t) {
         echo "Error al crear:".$t->getTraceAsString() ;
